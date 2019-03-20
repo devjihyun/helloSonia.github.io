999fdc5dc923307ee97bc232061776c92d0ffd2d
sonia.kim <sonia.kim@kakaocorp.com>
2019-03-20 17:55:37 +0900

    카운터 수정

--- js/counter.js
+++ js/counter.js
@@ -1,24 +1,40 @@
 "use strict";
 
-var up = document.getElementById('btnUp');
-var down = document.getElementById('btnDown');
-var reset = document.getElementById('btnReset');
-var numVal = document.getElementById('num').value;
+var minus = document.getElementById('btnMinus');
+var plus = document.getElementById('btnPlus');
+var i = 0;
 
-
-function numUp() {
-    document.getElementById('num').value = numVal++;
-}
+// - 버튼 클릭시 1씩 하락
 function numDown() {
-    document.getElementById('num').value = numVal--;
-}
-function numReset() {
-    document.getElementById('num').value = 0;
+    i--;
+    document.getElementById('num').value = i;
 }
 
+// +버튼 클릭시 1씩 상승
+function numUp() {
+    i++;
+    document.getElementById('num').value = i;
+}
 
-up.addEventListener('click', numUp);
-down.addEventListener('click', numDown);
-reset.addEventListener('click', numReset);
+minus.addEventListener('click', numDown);
+plus.addEventListener('click', numUp);
 
 
+// 숫자만 입력되게 하기
+function onlyNumber(event) {
+    event = event || window.event;
+    var keyID = (event.which) ? event.which : event.keyCode;
+    if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
+        return;
+    else
+        return false;
+}
+
+function removeChar(event) {
+    event = event || window.event;
+    var keyID = (event.which) ? event.which : event.keyCode;
+    if (keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
+        return;
+    else
+        event.target.value = event.target.value.replace(/[^0-9]/g, "");
+}
