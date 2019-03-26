93b37462f4d46edb475c5a7098b2f219ed646eaa
sonia.kim <sonia.kim@kakaocorp.com>
2019-03-26 11:27:56 +0900

    커밋테스트

--- js/counter.js
+++ js/counter.js
@@ -1,40 +1,16 @@
 "use strict";
 
-var minus = document.getElementById('btnMinus');
-var plus = document.getElementById('btnPlus');
-var i = 0;
+var num = 0;
+var count = document.getElementById("count");
 
-// - 버튼 클릭시 1씩 하락
-function numDown() {
-    i--;
-    document.getElementById('num').value = i;
+function plus(){
+    num++;
+    count.value = num;
 }
 
-// +버튼 클릭시 1씩 상승
-function numUp() {
-    i++;
-    document.getElementById('num').value = i;
-}
-
-minus.addEventListener('click', numDown);
-plus.addEventListener('click', numUp);
-
-
-// 숫자만 입력되게 하기
-function onlyNumber(event) {
-    event = event || window.event;
-    var keyID = (event.which) ? event.which : event.keyCode;
-    if ((keyID >= 48 && keyID <= 57) || (keyID >= 96 && keyID <= 105) || keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
-        return;
-    else
-        return false;
-}
-
-function removeChar(event) {
-    event = event || window.event;
-    var keyID = (event.which) ? event.which : event.keyCode;
-    if (keyID == 8 || keyID == 46 || keyID == 37 || keyID == 39)
-        return;
-    else
-        event.target.value = event.target.value.replace(/[^0-9]/g, "");
+function minus(){
+    if (num > 0){
+        num--;
+        count.value = num;
+    }
 }
