a0e63199f0a5e3731fc321a837ffc4e4cb62dc29
sonia.kim <sonia.kim@kakaocorp.com>
2019-03-29 15:30:15 +0900

    키보드 입력 방지 추가 숫자, 백스페이스만 입력 가능

--- js/counter.js
+++ js/counter.js
@@ -1,14 +1,34 @@
 "use strict";
 
-var num = 0;
-var count = document.getElementById("count");
+(function () {
+    var count = document.getElementById("count");
+    var num = 0;
+    
+    document.getElementById("count").value = num;
 
-function plus(){
-    num++;
-    count.value = num;
+    // +버튼 클릭 시 1씩 증가
+    document.getElementById("btnPlus").addEventListener("click", function plus() {
+        num++;
+        count.value = num;
+    });
+
+    // - 버튼 클릭 시 1씩 감소
+    document.getElementById("btnMinus").addEventListener("click", function minus() {
+        num--;
+        count.value = num;
+    });
+})();
+
+
+// 키보드 입력 방지
+function putNumber(event) {
+    event.preventDefault();
 }
-
-function minus(){
-    num--;
-    count.value = num;
+// 숫자만 입력되게 하기
+function putNumber(event) {
+    var code = event.keyCode || event.which;
+    if (code > 47 && code < 58) {} else if (code === 8) {
+        return;
+    } else
+        event.preventDefault();
 }
