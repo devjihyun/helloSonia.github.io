f6b7aa76ce33a74d53358a3be6882a59d5a3570f
sonia.kim <sonia.kim@kakaocorp.com>
2019-04-22 17:52:37 +0900

    -입력 이슈 해결?ㅋㅋ

--- js/counter.js
+++ js/counter.js
@@ -1,16 +1,10 @@
 "use strict";
 
 (function () {
+    //step1. +,-버튼으로 인풋 박스의 숫자 변경하기
     var inpCount = document.getElementById("inpNumCount");
     var countVal = 0;
     inpCount.value = countVal;
-
-    inpCount.addEventListener("keyup", function(event) {
-        validateNumber(event); /* 숫자만 입력되게 하는 함수 */
-        countVal = Number(inpCount.value); /* 문자열 숫자열로 변경 */
-        if (countVal !== 0) inpCount.value = countVal;
-    });
-
 
     // +버튼 클릭 시 1씩 증가
     function increase () {
@@ -24,16 +18,15 @@
         countVal--;
         inpCount.value = countVal;
     }
-    document.getElementById("btnMinus").addEventListener("click", decrease)
+    document.getElementById("btnMinus").addEventListener("click", decrease);
+    
+    //step2. 숫자 입력 및 입력 숫자 기준으로 버튼 제어
+    inpCount.addEventListener("keyup", function(event) {
+        countVal = Number(inpCount.value); /* 문자열 숫자열로 변경 */
+        
+        //ste3. 숫자만 입력되게 하기
+        var code = event.keyCode || event.which;
+        if (code == 8 || code == 46 || code == 37 || code == 39) return;
+        else event.target.value = event.target.value.replace(/[^0-9-]/g, '');
+    });
 })();
-
-
-// 숫자만 입력되게 하는 기능 함수
-// if (code > 47 && code < 58) 방법과 차이점
-// regex(정규표현식)에 대해 알아봅시다.
-// 현재 이슈 : 마이너스도 삭제됩니다. 키키 
-function validateNumber(event) {
-    var code = event.keyCode || event.which;
-    if (code == 8 || code == 46 || code == 37 || code == 39) return;
-    else event.target.value = event.target.value.replace(/[^0-9-]/g, '');
-}
