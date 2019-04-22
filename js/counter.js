ab66bc51503e7fced6dc961ee3919ddae920e5ef
sonia.kim <sonia.kim@kakaocorp.com>
2019-04-22 11:26:52 +0900

    숫자만 입력되게하기

--- js/counter.js
+++ js/counter.js
@@ -1,31 +1,39 @@
 "use strict";
 
 (function () {
-    var count = document.getElementById("count");
-    var i = count.value;
+    var inpCount = document.getElementById("inpNumCount");
+    var countVal = 0;
+    inpCount.value = countVal;
+
+    inpCount.addEventListener("keyup", function(event) {
+        validateNumber(event); /* 숫자만 입력되게 하는 함수 */
+        countVal = Number(inpCount.value); /* 문자열 숫자열로 변경 */
+        if (countVal !== 0) inpCount.value = countVal;
+    });
+
 
     // +버튼 클릭 시 1씩 증가
-    document.getElementById("btnPlus").addEventListener("click", function () {
-        i++;
-        count.value = i;
-    });
+    function increase () {
+        countVal++;
+        inpCount.value = countVal;
+    }
+    document.getElementById("btnPlus").addEventListener("click", increase);
 
     // - 버튼 클릭 시 1씩 감소
-    document.getElementById("btnMinus").addEventListener("click", function () {
-        i--;
-        count.value = i;
-    });
+    function decrease () {
+        countVal--;
+        inpCount.value = countVal;
+    }
+    document.getElementById("btnMinus").addEventListener("click", decrease)
 })();
 
 
- // 숫자만 입력되게 하기
- function putNumber(event) {
+// 숫자만 입력되게 하는 기능 함수
+// if (code > 47 && code < 58) 방법과 차이점
+// regex(정규표현식)에 대해 알아봅시다.
+// 현재 이슈 : 마이너스도 삭제됩니다. 키키 
+function validateNumber(event) {
     var code = event.keyCode || event.which;
-    if (code > 47 && code < 58) {
-        return;
-    } 
-    if (code === 8) {
-        return;
-    } 
-    event.preventDefault();
+    if (code == 8 || code == 46 || code == 37 || code == 39) return;
+    else event.target.value = event.target.value.replace(/[^0-9-]/g, '');
 }
