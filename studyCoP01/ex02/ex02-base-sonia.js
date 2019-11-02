6a56e3b3111658259088f7f78fcf32f961657dc5
sonia.kim <sonia.kim@kakaocorp.com>
2019-11-02 16:57:44 +0900

    셀렉트 박스

---
+++ studyCoP01/ex02/ex02-base-sonia.js
@@ -0,0 +1,16 @@
+var num1 = document.querySelector("#standardNum");
+var num2 = document.querySelector("#MultipliedyNum");
+
+
+var clickBtn = document.querySelector("#clickBtn");
+var inputNum = document.querySelector("#answer");
+
+clickBtn.addEventListener('click', function() {
+    num1 = Number;
+    num2 = Number;
+    inputNum = num1 * num2;
+
+    // for (var i = 0; i <= 100; i++) {
+    //     console.log(i);          
+    // }
+});
